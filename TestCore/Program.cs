68d2e19e8132232324e371591f89c16a728cab73
Mibokess <mikeboss@windowslive.com>
2019-01-10 23:59:55 +0100

    Initial commit

---
+++ TestCore/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Knyaz.Optimus;
+using Knyaz.Optimus.Dom.Elements;
+using Knyaz.Optimus.Graphics;
+using Console = System.Console;
+
+namespace TestCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(Ezivi).Wait();
+        }
+
+        private static async Task Ezivi()
+        {
+            var website = "https://www.ezivi.admin.ch/";
+
+            var engine = new Engine();
+
+            await engine.OpenUrl(website);
+                
+            var button = engine.Document.QuerySelector("a.[title='Einsatz suchen und vereinbaren']") as HtmlElement;
+            
+            button.Click();
+            
+            //await engine.OpenUrl("https://www.ezivi.admin.ch/ivy/faces/instances/16839CC238F32510/MainPage.xhtml");
+            
+            Console.WriteLine(engine.Document.InnerHTML);
+            //File.WriteAllText("test.html", engine.Document.OuterHTML);
+        }
+    }
+}
